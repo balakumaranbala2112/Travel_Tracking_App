1cebc20b6dc276f4b9197768c4ac47dacb87c5a9
balakumaranbala2112 <balakumaranbala2112@gmail.com>
2024-11-15 16:00:55 +0530

    Initial commit

---
+++ app/src/main/java/com/example/travelapp/RegisterActivity.kt
@@ -0,0 +1,172 @@
+package com.example.travelapp
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.core.content.ContextCompat
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+
+class RegisterActivity : ComponentActivity() {
+    private lateinit var databaseHelper: UserDatabaseHelper
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        databaseHelper = UserDatabaseHelper(this)
+        setContent {
+            RegistrationScreen(this, databaseHelper)
+        }
+    }
+}
+
+@Composable
+fun RegistrationScreen(context: Context, databaseHelper: UserDatabaseHelper) {
+
+    var username by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    var email by remember { mutableStateOf("") }
+    var error by remember { mutableStateOf("") }
+
+    val db = Firebase.firestore
+    val user = hashMapOf(
+        "username" to username,
+        "password" to password,
+        "email" to email
+    )
+
+// Add a new document with a generated ID
+    db.collection("users")
+        .add(user)
+        .addOnSuccessListener { documentReference ->
+            Log.d("LoginActivity", "DocumentSnapshot added with ID: ${documentReference.id}")
+        }
+        .addOnFailureListener { e ->
+            Log.w("LoginActivity", "Error adding document", e)
+        }
+    Column(
+        modifier = Modifier.fillMaxSize().background(Color.White),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+
+        Image(painterResource(id = R.drawable.tra), contentDescription = "")
+
+        Text(
+            fontSize = 36.sp,
+            fontWeight = FontWeight.ExtraBold,
+            fontFamily = FontFamily.Cursive,
+            text = "Register"
+        )
+
+        Spacer(modifier = Modifier.height(10.dp))
+        TextField(
+            value = username,
+            onValueChange = { username = it },
+            label = { Text("Username") },
+            modifier = Modifier
+                .padding(10.dp)
+                .width(280.dp)
+
+        )
+
+        TextField(
+            value = email,
+            onValueChange = { email = it },
+            label = { Text("Email") },
+            modifier = Modifier
+                .padding(10.dp)
+                .width(280.dp)
+        )
+
+        TextField(
+            value = password,
+            onValueChange = { password = it },
+            label = { Text("Password") },
+            visualTransformation = PasswordVisualTransformation(),
+            modifier = Modifier
+                .padding(10.dp)
+                .width(280.dp)
+        )
+
+
+        if (error.isNotEmpty()) {
+            Text(
+                text = error,
+                color = MaterialTheme.colors.error,
+                modifier = Modifier.padding(vertical = 16.dp)
+            )
+        }
+
+        Button(
+            onClick = {
+                if (username.isNotEmpty() && password.isNotEmpty() && email.isNotEmpty()) {
+                    val user = User(
+                        id = null,
+                        firstName = username,
+                        lastName = null,
+                        email = email,
+                        password = password
+                    )
+                    databaseHelper.insertUser(user)
+                    error = "User registered successfully"
+                    // Start LoginActivity using the current context
+                    context.startActivity(
+                        Intent(
+                            context,
+                            LoginActivity::class.java
+                        )
+                    )
+
+                } else {
+                    error = "Please fill all fields"
+                }
+            },
+            modifier = Modifier.padding(top = 16.dp)
+        ) {
+            Text(text = "Register")
+        }
+        Spacer(modifier = Modifier.width(10.dp))
+        Spacer(modifier = Modifier.height(10.dp))
+
+        Row() {
+            Text(
+                modifier = Modifier.padding(top = 14.dp), text = "Have an account?"
+            )
+            TextButton(onClick = {
+                context.startActivity(
+                    Intent(
+                        context,
+                        LoginActivity::class.java
+                    )
+                )
+            })
+
+            {
+                Spacer(modifier = Modifier.width(10.dp))
+                Text(text = "Log in")
+            }
+        }
+    }
+}
+private fun startLoginActivity(context: Context) {
+    val intent = Intent(context, LoginActivity::class.java)
+    ContextCompat.startActivity(context, intent, null)
+}
